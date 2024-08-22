3a22c51831875211410225f13fbcc23562412899
rahatmirazdev <rahatmiraz.dev@gmail.com>
2024-08-22 14:12:25 +0600

    Navbar section added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
